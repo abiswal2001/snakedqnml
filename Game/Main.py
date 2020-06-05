a0e7260fb05e3e37251a3928be6bd16df588b210
Preetham Sura <preethamss@berkeley.edu>
2020-06-04 19:50:29 -0700

    Timer works

--- Game/Main.py
+++ Game/Main.py
@@ -21,16 +21,25 @@ screen.fill(white)
 score = 0
 black = (0, 0, 0)
 font = pygame.font.Font('freesansbold.ttf', 32)
-text = font.render('Total Score: ' + str(score), True, black, white)
-text_rect = text.get_rect()
-text_rect.center = (windowWidth // 2, 20)
-screen.blit(text, text_rect)
+score_text, score_text_rect = 0, 0
+
+# Updates score every time the snake eats a something
+def updateScore():
+    global score_text, text_rect, score
+    score_text = font.render('Total Score: ' + str(score), True, black, white)
+    score_text_rect = score_text.get_rect()
+    score_text_rect.center = (windowWidth // 2, 20)
+    screen.blit(score_text, score_text_rect)
+    score += 1
+
+# Displays initial score of 0
+updateScore()
 
 # Snake Body
 snakeBody = [(20, 20)];
 
-# Key Press codes
-keys=pygame.key.get_pressed()
+# Start Timer
+pygame.time.set_timer(pygame.USEREVENT + 1, 1000)
 
 # Handles key presses
 def keyPresses():
@@ -39,28 +48,17 @@ def keyPresses():
 
     # Left key press
     if keys[pygame.K_LEFT]:
-        key_text = font.render("Left Key", True, black, white)
-        key_text_rect = text.get_rect()
-        key_text_rect.center = (windowWidth // 2, windowHeight // 2)
-        screen.blit(key_text, key_text_rect)
+        print("1")
     elif keys[pygame.K_RIGHT]:
-        key_text = font.render("Right Key", True, black, white)
-        key_text_rect = text.get_rect()
-        key_text_rect.center = (windowWidth // 2, windowHeight // 2)
-        screen.blit(key_text, key_text_rect)
+        print("2")
     elif keys[pygame.K_UP]:
-        key_text = font.render("Up Key", True, black, white)
-        key_text_rect = text.get_rect()
-        key_text_rect.center = (windowWidth // 2, windowHeight // 2)
-        screen.blit(key_text, key_text_rect)
+        print("3")
     elif keys[pygame.K_DOWN]:
-        key_text = font.render("Down Key", True, black, white)
-        key_text_rect = text.get_rect()
-        key_text_rect.center = (windowWidth // 2, windowHeight // 2)
-        screen.blit(key_text, key_text_rect)
+        print("4")
 
+# Updates snake every time a certain amount of time passes
 def updateSnake():
-    print("hello")
+    print("hi")
 
 # Keeps screen open until you close it
 running = True
@@ -71,6 +69,8 @@ while running:
     # Handles events
     for i in pygame.event.get():
         keyPresses()
+        if i.type == pygame.USEREVENT + 1:
+            updateScore();
         if i.type == pygame.QUIT:
             running = False
             pygame.quit()
