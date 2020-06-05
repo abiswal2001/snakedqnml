8a064867639346a5dcff653f54c0ddcbc2ea0e9d
Preetham Sura <preethamss@berkeley.edu>
2020-06-04 18:59:02 -0700

    Score can be displayed

--- Game/Main.py
+++ Game/Main.py
@@ -4,10 +4,36 @@
 import pygame
 import time
 
+# Initialize Game
 pygame.init()
 
+# Change Title of Game
+pygame.display.set_caption('Snake')
+
+
 # Setting up the window
-screen = pygame.display.set_mode([500, 500])
-for i in range(10):
-    print("Hello World" + str(time.perf_counter()))
-    time.sleep(1)
+windowWidth = 800
+windowHeight = 800
+screen = pygame.display.set_mode([windowWidth, windowHeight])
+white = [255, 255, 255]
+screen.fill(white)
+
+# Displaying Score
+score = 0
+black = (0, 0, 0)
+font = pygame.font.Font('freesansbold.ttf', 32)
+text = font.render('Total Score: ' + str(score), True, black, white)
+textRect = text.get_rect()
+textRect.center = (windowWidth // 2, 20)
+screen.blit(text, textRect)
+
+# Updates Pygame Display
+pygame.display.update()
+
+# Keeps screen open until you close it
+running = True
+while running:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            running = False
+            pygame.quit()
